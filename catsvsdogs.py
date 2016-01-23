a0ee1e9ad3726553cf291b0f2945b16c5080bd86
Ben Caine <bcaine0@gmail.com>
2016-01-22 20:26:54 -0500

    Pylint corrections and comments

--- catsvsdogs.py
+++ catsvsdogs.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python
+"""
+Solution to Spotify's catsvsdogs puzzle:
+https://labs.spotify.com/puzzles/
+
+This solution uses networkx to make dealing with graphs cleaner, and provides 
+functions for finding the maximum bipartite matching using the Hopcroft Karp 
+matching algorithm. 
+
+We create a bipartite graph where each node is a vote and each edge is a 
+conflict.
+
+We then use maximum bipartite matching to find the maximum number of edges
+we can with none of these edges sharing an endpoint. This number of edges
+tells us the number of nodes we would have to remove to seperate the graph,
+therefore satisfying the remaining voters (nodes). 
+
+The total_number_of_votes - len(max_matching) gives us the max number of
+satisfied voters.
+"""
 
 __author__ = "Ben Caine"
 __email__ = "Bcaine0@gmail.com"
@@ -12,18 +31,18 @@ from input_helpers import format_input, read_stdin
 
 class CatsVsDogs(object):
 
-    def __init__(self, data):
+    def __init__(self, instance):
         self.graph = nx.Graph()
         self.c, self.d, self.v = instance['header']
         self.votes = instance['votes']
 
-        assert(self.c >= 1 and self.c < 100)
-        assert(self.d >= 1 and self.d < 100)
-        assert(self.v >= 0 and self.v < 500)
-    
-    def _conflict(self, n1, n2):
-        vote1 = self.graph.node[n1]['votes']
-        vote2 = self.graph.node[n2]['votes']
+        assert self.c >= 1 and self.c < 100
+        assert self.d >= 1 and self.d < 100
+        assert self.v >= 0 and self.v < 500
+
+    def _conflict(self, node_1, node_2):
+        vote1 = self.graph.node[node_1]['votes']
+        vote2 = self.graph.node[node_2]['votes']
         # Check if what vote1 loves is what
         # vote2 hates, or vise versa
         if vote1[0] == vote2[1]:
@@ -33,42 +52,44 @@ class CatsVsDogs(object):
         return False
 
     def _remove_duplicates(self, matching):
+        # Sort the individual tuples so (1, 4) and (4, 1) are
+        # now both (1, 4), which makes deduping easy
         sorted_matches = [sorted(m) for m in matching.items()]
-        
         deduped = {}
         for match in sorted_matches:
-            k, v = match
-            deduped[k] = v
+            key, val = match
+            deduped[key] = val
         return deduped
 
     def max_satisfied_voters(self):
-        if self.v == 0: return 0
+        if self.v == 0:
+            return 0
 
         # Construct bipartite graph
         for i, vote in enumerate(self.votes):
             loves_val = 'cat' if 'C' in vote[0] else 'dog'
             self.graph.add_node(i, loves=loves_val, votes=vote)
-        
+
         # Create edges for each conflict
-        for n1 in self.graph.nodes():
-            for n2 in self.graph.nodes():
-                if n1 == n2:
+        for node_1 in self.graph.nodes():
+            for node_2 in self.graph.nodes():
+                if node_1 == node_2:
                     continue
-            
-                if self._conflict(n1, n2):
-                    self.graph.add_edge(n1, n2)
-                
+
+                if self._conflict(node_1, node_2):
+                    self.graph.add_edge(node_1, node_2)
+
         matching = bipartite.maximum_matching(self.graph)
         matching = self._remove_duplicates(matching)
-    
+
         return self.v - len(matching)
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     # Read data from stdin
     input_text = read_stdin()
     data = format_input(input_text)
 
-    for instance in data:
-        cats_vs_dogs = CatsVsDogs(instance)
+    for inst in data:
+        cats_vs_dogs = CatsVsDogs(inst)
         print cats_vs_dogs.max_satisfied_voters()
--- input_helpers.py
+++ input_helpers.py
@@ -1,5 +1,7 @@
-# Functions related to generating or reading in
-# data for the dogsvscats challenge
+"""
+Utility functions related to generating or reading
+in data for the Spotify dogsvscats challenge
+"""
 
 __author__ = "Ben Caine"
 __email__ = "Bcaine0@gmail.com"
@@ -9,12 +11,14 @@ import sys
 
 
 def read_stdin():
+    """Reads in STDIN and returns a list of lists"""
     data = sys.stdin.readlines()
     data = [d.strip().split(" ") for d in data]
     return data
 
 
 def format_input(data):
+    """Takes a list of lists and returns a list of dicts"""
     # Get rid of any starting or trailing empty strings
     if len(data[0]) == 0:
         data = data[1:]
@@ -23,27 +27,28 @@ def format_input(data):
 
     # Toss out the first row, which is unnecessary info
     data = data[1:]
-    
-    # Create structured data 
+
+    # Create structured data
     # format: [{"header": [c, d, v], "votes": [['C1', 'D1'], [..]]}, {...}]
     test_cases = []
-    
+
     next_header = 0
     for i, row in enumerate(data):
         if i == next_header:
             # If its not really a header, break
-            if len(row) < 3: break                
+            if len(row) < 3:
+                break
             # Grab header, and calculate when the next is
             header = [int(val) for val in row]
             next_header += (header[2] + 1)
             # Create new case and skip to next row (first vote)
             test_cases.append({"header": header})
             continue
-        
+
         # Add votes data to the case dictionary
         if "votes" in test_cases[-1]:
             test_cases[-1]["votes"].append(row)
         else:
             test_cases[-1]["votes"] = [row]
-        
+
     return test_cases
